6bc1a610cb47a4a0edb17ce7d60745df6dbda1c5
Matheus Degrande Garcia <matheus10degrande@gmail.com>
2025-07-09 12:18:29 -0300

    Adicionar HashRouter para corrigir roteamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/home";
 import ProductsPage from "./pages/ProductsPage";
